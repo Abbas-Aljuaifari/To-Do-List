2ed0ff498a54bfcb7ae9cba4a954e6e14c6a8aa4
fares aljuaifari <fares.aljuaifri@gmail.com>
2023-06-13 19:04:41 +0300

    Added all linters and checkrd all errors

--- script.js
+++ script.js
@@ -1,43 +1,40 @@
-const inputbox = document.getElementById("input-box");
-const lscontaner = document.getElementById("ls-contaner");
-
 const tasks = [
   {
-    description: "Task 1",
+    description: 'Task 1',
     completed: true,
     index: 1,
   },
   {
-    description: "Task 2",
+    description: 'Task 2',
     completed: true,
     index: 2,
   },
   {
-    description: "Task 3",
+    description: 'Task 3',
     completed: true,
     index: 3,
   },
 ];
 
 function renderTasks() {
-  const lscontaner = document.getElementById("ls-contaner");
-  lscontaner.innerHTML = "";
+  const lscontaner = document.getElementById('ls-contaner');
+  lscontaner.innerHTML = '';
 
   tasks.sort((a, b) => a.index - b.index); // Sort tasks by index
 
   tasks.forEach((task) => {
-    const li = document.createElement("li");
+    const li = document.createElement('li');
     li.innerHTML = task.description;
     if (task.completed) {
-      li.classList.add("completed");
+      li.classList.add('completed');
     }
 
-    const span = document.createElement("span");
-    span.innerHTML = "\u00d7";
+    const span = document.createElement('span');
+    span.innerHTML = '\u00d7';
     li.appendChild(span);
 
     lscontaner.appendChild(li);
   });
 }
 
-document.addEventListener("DOMContentLoaded", renderTasks);
+document.addEventListener('DOMContentLoaded', renderTasks);
